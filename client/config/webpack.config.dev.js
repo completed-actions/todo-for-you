956be289f47994f8f34662c8b86de631f4680012
Georgiy Starkov <gstarkov658@gmail.com>
2018-06-07 16:51:15 +0300

    Добавит import плагин и заменит cssnext

--- client/config/webpack.config.dev.js
+++ client/config/webpack.config.dev.js
@@ -39,13 +39,19 @@ module.exports = {
         test: /\.css$/,
         use: [
           'style-loader',
-          'css-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1
+            }
+          },
           {
             loader: 'postcss-loader',
             options: {
               ident: 'postcss',
               plugins: () => [
-                require('postcss-cssnext'),
+                require('postcss-import'),
+                require('postcss-preset-env'),
                 require('postcss-flexbugs-fixes')
               ]
             }
